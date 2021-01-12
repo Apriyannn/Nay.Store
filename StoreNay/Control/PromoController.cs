d334cfb28ca6952ddde620909f537ed41329fcc6
apriyan saputra <apriyansptr@gmail.com>
2021-01-12 09:04:51 +0700

    Add project files.

---
+++ StoreNay/Control/PromoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreNay.Isi;
+
+namespace StoreNay.Control
+{
+    class PromoController
+    {
+        private List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
---
+++ StoreNay/OnMenuSelected.cs
@@ -0,0 +1,6 @@
+﻿namespace StoreNay
+{
+    internal interface OnMenuSelected
+    {
+    }
+}
